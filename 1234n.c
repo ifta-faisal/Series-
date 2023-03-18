09d54a41da1efa74bb1c4bcb8e6655609368078f
Ifta Faisal <iftafaisal759@gmail.com>
2023-03-17 20:01:27 -0800

    Series

---
+++ 1234n.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main ()
+{
+    int n ;
+ printf("Enter tyhe value: ");
+ scanf("%d",&n);
+
+    for(int i=1 ; i<=n ; i++)
+    {
+  if (i==n)
+  {
+    printf(" %d",i);
+  }
+  else 
+  {
+    printf(" %d",i);
+  }
+    }
+
+    return 0;
+}
